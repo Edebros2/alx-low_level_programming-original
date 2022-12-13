92a6b262e9527b8b611aea027d7cd66c6f339c20
Ede Wisdom <edewisdom001@gmail.com>
2022-12-13 11:59:27 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,21 @@
 #include "main.h"
 
 /***
- * print_alphabet - prints the alpha in lowercase
+ * print_alphabet_x10 - prints alpha 10 times
  *
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
+	int ten;
 	char la;
 
-	for (la = 'a'; la <= 'z'; la++)
-		_putchar(la);
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
 
-	_putchar('\n');
 }
