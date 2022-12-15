bde6c11ebe50f8ff398549a20c5c02fd987cf469
Ede Wisdom <edewisdom001@gmail.com>
2022-12-15 12:39:08 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - gives the product of two integers a & b
+ * @x: variable 1
+ * @y: variable 2
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{	
+	return (a * b);
+}
